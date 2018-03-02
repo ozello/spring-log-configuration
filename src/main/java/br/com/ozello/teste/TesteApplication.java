48384955dcb68c22fcc679200e41cb37d5c70054
Alexandre Ozelo <alexandre.ozelo@padtec.com.br>
2018-03-02 09:18:02 -0300

    First repository commit

---
+++ src/main/java/br/com/ozello/teste/TesteApplication.java
@@ -0,0 +1,32 @@
+package br.com.ozello.teste;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class TesteApplication {
+
+  private static final Logger LOG = LoggerFactory.getLogger(TesteApplication.class);
+
+  @RequestMapping("/teste")
+  @ResponseBody
+  String home() {
+    for (int i = 0; i < 100; i++) {
+      LOG.info("[TAG_I_" + i + "] - Teste de log info.", "dafdfasdfsa");
+      LOG.error("[TAG_E_" + i + "] - Teste de log error.");
+      LOG.warn("[TAG_W_" + i + "] - Teste de log error.");
+      LOG.debug("[TAG_D_" + i + "] - Teste de log debug.");
+    }
+    return "Hello World!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(TesteApplication.class, args);
+  }
+}
